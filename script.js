28dc325c9a0954908829c70158bdbf6472066612
SoichiroShido <117181717+SoichiroShido@users.noreply.github.com>
2023-11-29 20:54:29 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ document.getElementById("purchaseButton").addEventListener("click", function() {
   setTimeout(function() {
       window.location.href = 'nextpage.html';
   }, 3000); // 3000ミリ秒（3秒）後に遷移
-  var audio = new Audio('sound.mp3'); // 音声ファイルへのパス
-    audio.play(); // 音声を再生
+  // var audio = new Audio('sound.mp3'); // 音声ファイルへのパス
+    // audio.play(); // 音声を再生
 });
