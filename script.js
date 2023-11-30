f8a8f907424278473d51b69ce41236f68bd5c78f
SoichiroShido <117181717+SoichiroShido@users.noreply.github.com>
2023-11-30 21:20:01 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById("purchaseButton").addEventListener("click", function() {
-  document.getElementById("confirmationMessage").textContent = "注文が完了しました、配達をお待ちください";
+  document.getElementById("confirmationMessage").textContent = "注文処理中、しばらくお待ちください";
   setTimeout(function() {
       window.location.href = 'nextpage.html';
   }, 3000); // 3000ミリ秒（3秒）後に遷移
